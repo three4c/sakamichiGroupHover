8916a66d5d3c4ca570a419226e4c9dad1103b184
three4c <three4c@gmail.com>
2019-01-26 04:30:11 +0900

    コードが雑なので後で修正

--- docs/js/index.js
+++ docs/js/index.js
@@ -7,26 +7,26 @@ const p3 = document.getElementById('p3');
 
 const container = document.getElementById('container')
 
-t1.addEventListener('mouseenter', () => {
+t1.addEventListener('mouseenter touchstart', () => {
   p1.classList.add("img-opacity")
 }, false);
 
-t1.addEventListener('mouseleave', () => {
+t1.addEventListener('mouseleave touchstart', () => {
   p1.classList.remove("img-opacity")
 }, false);
 
-t2.addEventListener('mouseenter', () => {
+t2.addEventListener('mouseenter touchstart', () => {
   p2.classList.add("img-opacity")
 }, false);
 
-t2.addEventListener('mouseleave', () => {
+t2.addEventListener('mouseleave touchstart', () => {
   p2.classList.remove("img-opacity")
 }, false);
 
-t3.addEventListener('mouseenter', () => {
+t3.addEventListener('mouseenter touchstart', () => {
   p3.classList.add("img-opacity")
 }, false);
 
-t3.addEventListener('mouseleave', () => {
+t3.addEventListener('mouseleave touchstart', () => {
   p3.classList.remove("img-opacity")
 }, false);
