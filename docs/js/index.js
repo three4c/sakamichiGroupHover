6dd34b1e37af42e3018752f664f38912055cb895
three4c <three4c@gmail.com>
2019-01-27 01:35:56 +0900

    2つのページを作成

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,32 +1,13 @@
-const t1 = document.getElementById('t1');
-const t2 = document.getElementById('t2');
-const t3 = document.getElementById('t3');
-const p1 = document.getElementById('p1');
-const p2 = document.getElementById('p2');
-const p3 = document.getElementById('p3');
+let textParent = document.getElementById('container__text');
+let textChildren = textParent.children;
+let imgParent = document.getElementById('container__img');
+let array = ['p1', 'p2', 'p3']
 
-const container = document.getElementById('container')
-
-t1.addEventListener('mouseenter', () => {
-  p1.classList.add("img-opacity")
-}, false);
-
-t1.addEventListener('mouseleave', () => {
-  p1.classList.remove("img-opacity")
-}, false);
-
-t2.addEventListener('mouseenter', () => {
-  p2.classList.add("img-opacity")
-}, false);
-
-t2.addEventListener('mouseleave', () => {
-  p2.classList.remove("img-opacity")
-}, false);
-
-t3.addEventListener('mouseenter', () => {
-  p3.classList.add("img-opacity")
-}, false);
-
-t3.addEventListener('mouseleave', () => {
-  p3.classList.remove("img-opacity")
-}, false);
+for (let i = 0; i < textChildren.length; i++) {
+  textChildren[i].addEventListener('mouseenter', () => {
+    imgParent.classList.add(array[i], 'on');
+  }, false);
+  textChildren[i].addEventListener('mouseleave', () => {
+    imgParent.classList.remove(array[i], 'on');
+  }, false);
+}
