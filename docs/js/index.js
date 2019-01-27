1a75bab78093c380131eef507cd14836d4105bfd
three4c <three4c@gmail.com>
2019-01-27 13:06:24 +0900

    mixinしまくり

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,13 +1,20 @@
 let textParent = document.getElementById('container__text');
 let textChildren = textParent.children;
 let imgParent = document.getElementById('container__img');
-let array = ['p1', 'p2', 'p3']
+let imgChildren = imgParent.children
 
 for (let i = 0; i < textChildren.length; i++) {
   textChildren[i].addEventListener('mouseenter', () => {
-    imgParent.classList.add(array[i], 'on');
+    imgChildren[i].classList.add('on');
   }, false);
   textChildren[i].addEventListener('mouseleave', () => {
-    imgParent.classList.remove(array[i], 'on');
+    imgChildren[i].classList.remove('on');
+  }, false);
+
+  textChildren[i].addEventListener('touchstart', () => {
+    imgChildren[i].classList.add('on');
+  }, false);
+  textChildren[i].addEventListener('touchend', () => {
+    imgChildren[i].classList.remove('on');
   }, false);
 }
